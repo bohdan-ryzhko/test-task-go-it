bd92788981aaa276f86dd8377bc101b5ab28d9b1
Bogdan Ryzhko <m3meizumini@gmail.com>
2023-05-20 16:21:14 +0300

    add srtict mode, fix 404.html 04

--- src/index.tsx
+++ src/index.tsx
@@ -6,25 +6,22 @@ import reportWebVitals from './reportWebVitals';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from './redux/store';
 import App from './components/App/App';
-
-// import React from 'react';
+import React from 'react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/test-task-go-it">
-        <App />
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>
-  // {/* </React.StrictMode> */}
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/test-task-go-it">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
-
-
 
 reportWebVitals();
