c1e42f43ed130eda3d273678d3e76fc0bc7d24b4
Bogdan Ryzhko <m3meizumini@gmail.com>
2023-05-20 03:30:13 +0300

    add lazy loading

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,47 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import App from './components/App/App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.scss';
-import { persistor, store } from './redux/store';
 import reportWebVitals from './reportWebVitals';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Layout } from './components/Layout/Layout';
+import { persistor, store } from './redux/store';
+import { Suspense, lazy } from 'react';
+import { NotFound } from './pages/NotFound/NotFound';
+
 // import React from 'react';
+
+const LazyHome = lazy(() => import('./pages/Home/Home'));
+const LazyTweets = lazy(() => import('./pages/Tweets/Tweets'));
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   // <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/test-task-go-it">
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter basename="/test-task-go-it">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index path="/" element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <LazyHome />
+              </Suspense>} />
+              <Route path="/tweets" element={
+                <Suspense fallback={<div>Loading...</div>}>
+                  <LazyTweets />
+                </Suspense>} />
+              <Route element={<NotFound />} />
+            </Route>
+          </Routes>
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
   // {/* </React.StrictMode> */}
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
 reportWebVitals();
