46f0284cee4358f6e1db154f914124289d57bb16
Bogdan Ryzhko <m3meizumini@gmail.com>
2023-05-20 03:44:40 +0300

    Refactor index.tsx and App.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,27 @@
 import sass from "./App.module.scss";
 import { Route, Routes } from "react-router-dom";
-import { FC } from "react";
-import Home from "../../pages/Home/Home";
-import Tweets from "../../pages/Tweets/Tweets";
+import { FC, Suspense, lazy } from "react";
 import { Layout } from "../Layout/Layout";
+import { NotFound } from "../../pages/NotFound/NotFound";
+
+const LazyHome = lazy(() => import("../../pages/Home/Home"));
+const LazyTweets = lazy(() => import("../../pages/Tweets/Tweets"));
 
 const App: FC = () => {
   return (
     <div className={sass.App}>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="/tweets" element={<Tweets />} />
-          </Route>
+            <Route index path="/" element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <LazyHome />
+              </Suspense>} />
+              <Route path="/tweets" element={
+                <Suspense fallback={<div>Loading...</div>}>
+                  <LazyTweets />
+                </Suspense>} />
+              <Route element={<NotFound />} />
+            </Route>
         </Routes>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
 import { PersistGate } from 'redux-persist/integration/react';
-import { Layout } from './components/Layout/Layout';
 import { persistor, store } from './redux/store';
-import { Suspense, lazy } from 'react';
-import { NotFound } from './pages/NotFound/NotFound';
+import App from './components/App/App';
 
 // import React from 'react';
-
-const LazyHome = lazy(() => import('./pages/Home/Home'));
-const LazyTweets = lazy(() => import('./pages/Tweets/Tweets'));
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -23,19 +18,7 @@ root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter basename="/test-task-go-it">
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index path="/" element={
-              <Suspense fallback={<div>Loading...</div>}>
-                <LazyHome />
-              </Suspense>} />
-              <Route path="/tweets" element={
-                <Suspense fallback={<div>Loading...</div>}>
-                  <LazyTweets />
-                </Suspense>} />
-              <Route element={<NotFound />} />
-            </Route>
-          </Routes>
+        <App />
       </BrowserRouter>
     </PersistGate>
   </Provider>
