37dc1d7a4f59fc8e661485c95acc33176b7de8c5
Bogdan Ryzhko <m3meizumini@gmail.com>
2023-05-21 13:36:44 +0300

    remove strict mode, remove justify-content tweetsList

--- src/index.tsx
+++ src/index.tsx
@@ -6,22 +6,19 @@ import reportWebVitals from './reportWebVitals';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from './redux/store';
 import App from './components/App/App';
-import React from 'react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/test-task-go-it">
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter basename="/test-task-go-it">
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
 
 reportWebVitals();
